64eb8e53dc23201d6125ef26c58caa356b163d63
Olga Pankrashina <olyaolya2713@yandex.ru>
2024-06-18 17:47:28 +0300

    add: add mocks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import style from './App.module.scss';
 import { Main } from './components/pages';
 
 export default function App() {
-  return <Main />;
+  return (
+    <div className={style.app}>
+      <Main />
+    </div>
+  );
 }
